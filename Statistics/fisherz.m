5c10cefc454791121ba8c2116b84639fbd242338
JaerongAhn <jaerongahn@gmail.com>
2020-08-02 00:28:24 -0400

    .

--- Statistics/fisherz.m
+++
@@ -1,6 +0,0 @@
-function z=fisherz(r)
-
-% Z=fisherz(R)
-%
-
-z=0.5*log((1+r)./(1-r));
--- Statistics/sem.m
+++
@@ -1,6 +0,0 @@
-function y = sem(x,dim)
-
-%% calculates the standard error of the mean (SEM) of vector x by AJR 2013/06/25
-
-y = nanstd(x,dim) / sqrt(numel(x));
-end
